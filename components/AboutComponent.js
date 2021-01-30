39d0c8cd8c84032a1c9d90a47561ba9256671891
AJ <ancheetah@users.noreply.github.com>
2021-01-30 12:50:09 -0500

    Task 3: add content to about page

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,12 +1,32 @@
 import React, { Component } from 'react';
-import { View, Text, ScrollView } from 'react-native';
-import { Card } from 'react-native-elements';
+import { Text, ScrollView, FlatList } from 'react-native';
+import { Card, ListItem } from 'react-native-elements';
+import { PARTNERS } from '../shared/partners';
+
+function Mission() {
+    return (
+        <Card title="Our Mission" wrapperStyle={{margin: 20}} >
+            <Text>
+                We present a curated database of the best campsites 
+                in the vast woods and backcountry of the World Wide 
+                Web Wilderness. We increase access to adventure for 
+                the public while promoting safe and respectful use 
+                of resources. The expert wilderness trekkers on our 
+                staff personally verify each campsite to make sure 
+                that they are up to our standards. We also present a 
+                platform for campers to share reviews on campsites 
+                they have visited with each other.
+            </Text>
+        </Card>
+    );
+}
 
 class About extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
+            partners: PARTNERS
         };
     }
 
@@ -15,8 +35,28 @@ class About extends Component {
     }
 
     render() {
+
+        const renderPartner = ({item}) => {
+            return (
+                <ListItem
+                    title={item.name}
+                    subtitle={item.description}
+                    leftAvatar={{ source: require('./images/bootstrap-logo.png')}}
+                />
+            );
+        };
+
         return (
-            <ScrollView></ScrollView>
+            <ScrollView>
+                <Mission/>
+                <Card title="Community Partners" wrapperStyle={{margin: 20}}>
+                <FlatList
+                    data={this.state.partners}
+                    renderItem={renderPartner}
+                    keyExtractor={item => item.id.toString()}
+                />
+                </Card>
+            </ScrollView>
         );
     }
 }
