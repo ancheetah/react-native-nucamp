53c5ed46d83472a9f72995c2250ef68c1b50454f
AJ <ancheetah@users.noreply.github.com>
2021-01-30 12:29:09 -0500

    Task 2: add content to contact page

--- components/ContactComponent.js
+++ components/ContactComponent.js
@@ -16,7 +16,19 @@ class Contact extends Component {
 
     render() {
         return (
-            <ScrollView></ScrollView>
+            <ScrollView>
+                <Card
+                    title="Contact Information"
+                    wrapperStyle={{margin: 20}}
+                >
+                    <Text>1 Nucamp Way</Text>
+                    <Text>Seattle, WA 98001</Text>
+                    <Text style={{marginBottom: 10}}>U.S.A.</Text>
+
+                    <Text>Phone: 1-206-555-1234</Text>
+                    <Text>Email: campsites@nucamp.co</Text>
+                </Card>
+            </ScrollView>
         );
     }
 }
