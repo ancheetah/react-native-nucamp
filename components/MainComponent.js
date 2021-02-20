fd1418223dfd3e3790f57506f726b2884bb256f1
AJ <ancheetah@users.noreply.github.com>
2021-02-19 21:27:33 -0500

    secure store - remember login info

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -6,6 +6,7 @@ import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import Reservation from './ReservationComponent';
 import Favorites from './FavoritesComponent';
+import Login from './LoginComponent';
 
 // Navigation
 // expo install react-navigation@4
@@ -177,6 +178,29 @@ const FavoritesNavigator = createStackNavigator(
     }
 );
 
+const LoginNavigator = createStackNavigator(
+    {
+        Login: { screen: Login }
+    },
+    {
+        defaultNavigationOptions: ({navigation}) => ({
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            },
+            headerLeft: <Icon
+                name='sign-in'
+                type='font-awesome'
+                iconStyle={styles.stackIcon}
+                onPress={() => navigation.toggleDrawer()}
+            />
+        })
+    }
+);
+
 const CustomDrawerContentComponent = props => (
     <ScrollView>
         <SafeAreaView 
@@ -197,6 +221,19 @@ const CustomDrawerContentComponent = props => (
 
 const MainNavigator = createDrawerNavigator(
     {
+        Login: {
+            screen: LoginNavigator,
+            navigationOptions: {
+                drawerIcon: ({tintColor}) => (
+                    <Icon
+                        name='sign-in'
+                        type='font-awesome'
+                        size={24}
+                        color={tintColor}
+                    />
+                )
+            }
+        },
         Home: { screen: HomeNavigator },
         Directory: { screen: DirectoryNavigator },
         Reservation: {
@@ -231,6 +268,7 @@ const MainNavigator = createDrawerNavigator(
         Contact: { screen: ContactNavigator }
     },
     {
+        initialRouteName: 'Home',   // First page that loads should be Home, not Login
         drawerBackgroundColor: '#CEC8FF',
         contentComponent: CustomDrawerContentComponent
     }
