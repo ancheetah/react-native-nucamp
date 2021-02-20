5f86def2d94062a672b3ead02c1a404704f8df89
AJ <ancheetah@users.noreply.github.com>
2021-02-19 21:53:34 -0500

    social sharing

--- components/CampsiteInfoComponent.js
+++ components/CampsiteInfoComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Text, View, ScrollView, FlatList,
-        Modal, Button, StyleSheet, Alert, PanResponder } from 'react-native';
+        Modal, Button, StyleSheet, 
+        Alert, PanResponder, Share } from 'react-native';
 import { Card, Icon, Input, Rating } from 'react-native-elements';
 import * as Animatable from 'react-native-animatable';
 
@@ -23,6 +24,16 @@ const mapStateToProps = state => {
 const mapDispatchToProps = {
     postFavorite: campsiteId => (postFavorite(campsiteId)),
     postComment: (campsiteId, rating, author, text) => (postComment(campsiteId, rating, author, text))
+};
+
+const shareCampsite = (title, message, url) => {
+    Share.share({
+        title: title,
+        message: `${title}: ${message} ${url}`,
+        url: url
+    },{
+        dialogTitle: 'Share ' + title
+    });
 };
 
 function RenderComments({comments}) {
@@ -139,6 +150,14 @@ function RenderCampsite(props) {
                             raised
                             reverse
                             onPress={() => props.onShowModal()}
+                        />
+                        <Icon
+                            name={'share'}
+                            type='font-awesome'
+                            color='#5637DD'
+                            raised
+                            reverse
+                            onPress={() => shareCampsite(campsite.name, campsite.description, baseUrl + campsite.image)} 
                         />
                     </View>
                 </Card>
